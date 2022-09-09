b2fddd767fcf26c1bd51d13d50e4a145e19c1864
MelisaOzmeral <ozmeralmelisa@gmail.com>
2022-09-09 18:14:52 +0300

    Visual and session processing

---
+++ CoreDemo/ViewModel/ProvinceViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewModel
+{
+  
+    public class ProvinceViewModel
+    {
+        public ProvinceViewModel()
+        {
+            this.ProvinceList = new List<SelectListItem>();
+            ProvinceList.Add(new SelectListItem { Text = "Seçiniz", Value = "" });
+        }
+
+        public int Province { get; set; }
+        public List<SelectListItem> ProvinceList { get; set; }
+    }
+}
--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,8 +15,12 @@ namespace EntityLayer.Concrete
         public string WriterName { get; set; }
         public string WriterAbout { get; set; }
         public string WriterImage { get; set; }
-        public string WriterMail { get; set; }
-        public string WriterPassword { get; set; }
+        public string WriterMail { get; set; }    
+        public string WriterPassword { get; set; }          
+        [NotMapped]      
+        public string ConfirmPassword { get; set; }
         public bool WriterStatus { get; set; }
+        public List<Blog> Blogs { get; set; }
+
     }
 }
